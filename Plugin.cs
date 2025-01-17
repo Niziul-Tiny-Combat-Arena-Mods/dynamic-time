406c880fe4920163565a3ebcd3c7e260965348eb
niziul <NiziulLuizin@Gmail.com>
2025-01-17 16:32:44 -0300

    Release version [0.1.0.0]

---
+++ Plugin.cs
@@ -0,0 +1,84 @@
+﻿using BepInEx;
+using UnityEngine;
+using Falcon.World;
+using Falcon.Game2.UI;
+using System.Collections.Generic;
+
+namespace dynamic_time;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID,
+             MyPluginInfo.PLUGIN_NAME,
+             MyPluginInfo.PLUGIN_VERSION)]
+[BepInProcess("Arena.exe")]
+public class Plugin : BaseUnityPlugin
+{
+    internal static Dictionary<TimeOfDay, float> timeOfDayPresets;
+
+    internal static TimeOfDay currentTimeOfDay;
+
+    private void Awake()
+    {
+        currentTimeOfDay
+            = TimeOfDay.Dawn;
+
+        timeOfDayPresets
+            = new Dictionary<TimeOfDay, float>
+        {
+            {
+                TimeOfDay.Dawn,
+                6f
+            },
+            {
+                TimeOfDay.Morning,
+                9f
+            },
+            {
+                TimeOfDay.Noon,
+                12f
+            },
+            {
+                TimeOfDay.Afternoon,
+                15f
+            },
+            {
+                TimeOfDay.Evening,
+                17.75f
+            },
+            {
+                TimeOfDay.Night,
+                22f
+            }
+        };
+    }
+
+    void Update()
+    {
+        if (SingletonMonobehaviour<QuickMissionBuilder>.Instance != null)
+        {
+            if (Environment.Instance.TODTimespan.Hours != timeOfDayPresets[currentTimeOfDay]
+                &&
+                Environment.Instance.Timescale == 0f)
+            {
+                Environment.Instance.SetTimeOfDayPreset(currentTimeOfDay);
+            }
+
+            if (Input.GetKeyUp(KeyCode.Alpha0))
+            {
+                currentTimeOfDay
+                    = (int)currentTimeOfDay++ < 5 ? currentTimeOfDay : currentTimeOfDay = 0;
+            }
+
+            if (Input.GetKeyUp(KeyCode.Alpha9))
+            {
+                if (Environment.Instance.Timescale == 0f)
+                {
+                    Environment.Instance.Timescale = 48f;
+                }
+                else
+                {
+                    Environment.Instance.Timescale = 0f;
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,60 @@
+<p><strong>About the mod</strong></p>
+
+<p>
+    Dynamic Time Plugin is an BepInEx plugin for Arena.exe that dynamically adjusts and controls the in-game time of day.
+</p>
+
+<p><strong>Features include:</strong></p>
+
+<ul>
+    <li>
+        <strong>Time Presets:</strong> Predefined time-of-day stages 
+        (Dawn, Morning, Noon, Afternoon, Evening, Night) with associated in-game hours.
+    </li>
+    <li>
+        <strong>Manual Time Control:</strong> Switch between time-of-day stages 
+        using a single key press (default: <strong>0</strong>).
+    </li>
+    <li>
+        <strong>Timescale Management:</strong> Toggle between paused and accelerated time modes 
+        with another key press (default: <strong>9</strong>).
+    </li>
+    <li>
+        <strong>Dynamic Synchronization:</strong> Ensures the game environment adheres to the 
+        current time preset when time is paused.
+    </li>
+</ul>
+
+<p><strong>Mandatory Prerequisites</strong></p>
+
+<ul>
+    <li>
+        <a href="https://store.steampowered.com/app/1347550/Tiny_Combat_Arena/" rel="nofollow">
+            Tiny Combat Arena
+        </a>
+        &nbsp;(0.15.2.3) or high
+    </li>
+    <li>
+        <a href="https://github.com/BepInEx/BepInEx/releases">
+            BepInEx
+        </a>
+        &nbsp;(5.4.21)&nbsp;LTS (x86) version
+    </li>
+</ul>
+
+<p><strong>Installation</strong></p>
+
+<ol>
+    <li>Extract the "dynamic-time.dll" file from the .rar archive;</li>
+    <li>Place the extracted file in your "BepInEx\plugins" folder;</li>
+</ol>
+
+<p><strong>Changelog</strong></p>
+
+<ul>
+    <li>[0.1.0.0]
+        <ul>
+            <li>Initial release;</li>
+        </ul>
+    </li>
+</ul>
